eb3e354e47752543577d098014ccaf8c1bd2551c
Leander Christmann <leander.christmann@proton.me>
2024-02-08 21:36:43 +0100

    make products page DYNAMIC (MongoDB + SSG)

--- app/products/page.js
+++ app/products/page.js
@@ -3,7 +3,33 @@ import Header from "@/components/header";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Products() {
+import { MongoClient } from "mongodb";
+
+async function getProducts() {
+  let mongoClient;
+
+  try {
+    mongoClient = new MongoClient(process.env.DB_URI);
+    await mongoClient.connect();
+    const db = mongoClient.db('woodruff-woodstore');
+
+    const cursor = db.collection('products')
+      .find({})
+      .project({ id: 1, name: 1, imageSrc: 1, price: 1, _id: 0 })
+      .sort({ id: 1 });
+
+    return await cursor.toArray();
+    
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await mongoClient.close();
+  }
+}
+
+export default async function Products() {
+  const products = await getProducts();
+
   return (
     <>
       <Header />
@@ -12,63 +38,15 @@ export default function Products() {
 
           <div className="grid gap-y-10 grid-cols-4 gap-x-8">
 
-            <Link href="/products/1">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-1.jpg" alt="Earthen Bottle" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
-            </Link>
-            <Link href="/products/2">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-2.jpg" alt="Nomad Tumbler" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-            </Link>
-            <Link href="/products/3">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-3.jpg" alt="Focus Paper Refill" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
-            </Link>
-            <Link href="/products/4">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-4.jpg" alt="Focus Paper Refill" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-            </Link>
-
-            <Link href="/products/5">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-5.jpg" alt="Earthen Bottle" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
-            </Link>
-            <Link href="/products/6">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-6.jpg" alt="Nomad Tumbler" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-            </Link>
-            <Link href="/products/7">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-7.jpg" alt="Focus Paper Refill" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$89</p>
-            </Link>
-            <Link href="/products/8">
-              <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
-                <Image src="/images/product-8.jpg" alt="Focus Paper Refill" fill className="hover:opacity-75 object-cover object-center" priority/>
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">$35</p>
-            </Link>
+            {products.map(product => (
+              <Link href={`/products/${product.id}`}>
+                <div className="w-full overflow-hidden rounded-lg bg-gray-200 aspect-h-7 aspect-w-7">
+                  <Image src={product.imageSrc} alt={product.name} fill className="hover:opacity-75 object-cover object-center" priority/>
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
+              </Link>
+            ))}
 
           </div>
 
